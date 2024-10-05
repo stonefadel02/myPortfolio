bab94f156c0fef21928adcdd70ea7b88f32f1776
stonefadel02 <stone.afedjou@epitech.eu>
2024-10-05 02:54:59 +0100

    update

--- js/main.js
+++ js/main.js
@@ -880,92 +880,89 @@ form.addEventListener("submit", handleSubmit);
 
 const projectData = [
   {
-    title: "PassGen",
+    title: "POSTIT",
     subTitle:
       "You can easily generate secure and random passwords using this simple tool.",
-    thumbnail: "../img/projects-default.jpg",
+    thumbnail: "../img/postit.png",
     description:
-      "PassGen is a Python CLI tool for generating secure, random passwords from the command line. It allows you to easily create strong and unique passwords for your accounts, with a variety of customization options. Simply enter a few commands and PassGen will do the rest, providing you with a secure password to use with confidence. Fast and efficient, PassGen is the perfect tool for anyone who needs to generate passwords on the fly.",
-    techStack: ["Python"],
-    srcURL: "https://github.com/LakshanRukantha/PassGen",
+      "POSTIT is a website where you can post tasks to do, modify them and delete them if necessary. The data is managed by an api and the technology used is Vuejs.",
+    techStack: ["Vuejs","Tailwind CSS"],
+    srcURL: "",
   },
   {
-    title: "CutLink",
+    title: "TWP",
     subTitle: "Here you have full control over your links.",
-    thumbnail: "../img/thumbnails/cutlink-thumbnail.png",
+    thumbnail: "../img/TWP.png",
     description:
-      "CutLink is a modern and user-friendly web application built with React and Material UI. Our platform offers a complete solution for link shortening. With CutLink, you can create short, branded links that are easy to share. Try CutLink today and experience the power of a smarter link shortning platform!",
-    techStack: ["React", "MUI"],
-    srcURL: "https://cut-link.netlify.app/",
+      "TWP is a task management website like Trello. It's a platform that lets you add tasks to a category and comment on them. My application also lets you drag and drop elements. It's great.  All managed with Vuejs",
+    techStack: ["Vuejs", "Tailwind CSS"],
+    srcURL: "",
   },
   {
-    title: "MindMate",
+    title: "Yowl",
     subTitle:
       "Unlock the power of your mind with our AI-Driven mental health web application",
-    thumbnail: "../img/thumbnails/mindmate-thumbnail.png",
+    thumbnail: "../img/yawl.png",
     description:
-      "MindMate is a web application that provides mental health solutions using artificial intelligence powered by OpenAI. It is designed to help individuals struggling with mental health issues to improve their well-being and overall quality of life. The app provides personalized and confidential support to users by analyzing their symptoms, emotions, and behavior. MindMate uses advanced algorithms to identify patterns in user data and provide them with tailored recommendations for managing their mental health.",
-    techStack: ["Next.js", "React", "Node.js"],
-    srcURL: "https://mind-mate.vercel.app/",
+      "Yowl is an application that lets you comment on everything on the Internet. Yowl breaks the barriers and restrictions of any community. With Yawl, everyone is free to express themselves. On Yawl you can publish content, comment, see your kpi, manage your user profile and more.",
+    techStack: ["Vue.js", "Laravel", "Tailwind", "Docker"],
+    srcURL: "",
   },
   {
     title: "My Shop",
-    subTitle:
-      "Unlock the power of your mind with our AI-Driven mental health web application",
-    thumbnail: "../img/thumbnails/mindmate-thumbnail.png",
+    thumbnail: "../img/prod.png",
     description:
-      "MindMate is a web application that provides mental health solutions using artificial intelligence powered by OpenAI. It is designed to help individuals struggling with mental health issues to improve their well-being and overall quality of life. The app provides personalized and confidential support to users by analyzing their symptoms, emotions, and behavior. MindMate uses advanced algorithms to identify patterns in user data and provide them with tailored recommendations for managing their mental health.",
-    techStack: ["Next.js", "React", "Node.js"],
-    srcURL: "https://mind-mate.vercel.app/",
+      "My shop is an e-commerce site where you can search for and purchase products. The site has an admin area for managing users and products.",
+    techStack: ["PHP", "HTML/CSS"],
   },
   {
-    title: "GitTrack",
+    title: "FREEADS",
     subTitle: "Gain insights and strengthen your GitHub connections",
-    thumbnail: "../img/thumbnails/gittrack-thumbnail.png",
+    thumbnail: "../img/free.png",
     description:
-      "GitTrack - an innovative web application designed to empower GitHub users with powerful network analysis tools. With GitTrack, you can effortlessly track and analyze your followers, identifying those who are not reciprocating the follow. Take control of your GitHub presence, strengthen your connections, and optimize your network. Gain valuable insights and make informed decisions to build a strong and engaged community.",
-    techStack: ["Next.js", "TypeScript", "Tailwind CSS"],
-    srcURL: "https://gittrack.vercel.app/",
+      "FREEADS is a clothing sales site where you can browse and buy products. There's an admin area to manage users and add products. ",
+    techStack: ["Laravel", "Bootstrap"],
+    srcURL: "",
   },
   {
-    title: "My Battery",
+    title: "Dashboard",
     subTitle:
       "Simple website that display the battery status of your device and the charging status.",
-    thumbnail: "../img/thumbnails/my-battery-thumbnail.png",
+    thumbnail: "../img/dash.png",
     description:
-      "My Battery is a simple and minimalistic web application that displays your battery percentage, charging status, and whether the battery is low or full. It's important to note that this is a website-based application.",
-    techStack: ["JavaScript", "HTML", "Tailwind CSS", "CSS"],
-    srcURL: "https://mybattery.vercel.app/",
+      "Welcome to the Dashboard project! This application is a customizable online dashboard that integrates various services through widgets. Built with a modern tech stack, this project allows users to create a personalized dashboard experience by selecting and using various widgets.The Dashboard project is designed to provide users with a flexible and interactive dashboard. Users can sign in or log in to customize their dashboard by selecting the widgets they want to use. Each widget corresponds to a specific service, offering a range of functionalities",
+    techStack: ["ReactJs", "ExpressJs", "NestJs", "Tailwind CSS", "Docker", "Jenkins", "MongoDB"],
+    srcURL: "",
   },
   {
-    title: "SysInfo",
+    title: "My Rotten Tomatoes",
     subTitle: "Web based terminal emulator",
-    thumbnail: "../img/thumbnails/sys-info-thumbnail.png",
+    thumbnail: "../img/rotten.png",
     description:
-      "SysInfo is a web based terminal emulator which you can run basic commands like help, clear, battery, weather and etc.",
-    techStack: ["JavaScript", "HTML", "CSS", "Tailwind CSS"],
-    srcURL: "https://sys-info.vercel.app/",
-  },
-  {
-    title: "Project Title",
-    subTitle:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima nobis quia et.",
-    thumbnail: "../img/projects-default.jpg",
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi placeat magnam eveniet accusamus tenetur maxime aspernatur deleniti rerum praesentium ducimus minima facilis consectetur expedita, mollitia molestias qui dolorem quam laudantium. Repellendus sunt harum fugiat natus. dolor sit amet consectetur, adipisicing elit. Minima nobis quia et.",
-    techStack: ["Stack"],
+      "My rotten tomatoes is a two-part site. A user section where you can browse movies, comment and rate them, add them as favorites and filter them. An admin panel that runs an api and manages movies and users. Users can be admins or not. The site was built with Nextjs",
+    techStack: ["Nextjs", "Docker","MongoDB", "Tailwind CSS"],
     srcURL: "",
   },
   {
-    title: "Project Title",
+    title: "Trelltech",
     subTitle:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima nobis quia et.",
-    thumbnail: "../img/projects-default.jpg",
+    thumbnail: "../img/trell.png",
     description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi placeat magnam eveniet accusamus tenetur maxime aspernatur deleniti rerum praesentium ducimus minima facilis consectetur expedita, mollitia molestias qui dolorem quam laudantium. Repellendus sunt harum fugiat natus. dolor sit amet consectetur, adipisicing elit. Minima nobis quia et.",
-    techStack: ["Stack"],
+      "Trelltech is a mobile task management application like Trello that uses the Trello api and offers a connection via Trello.",
+    techStack: ["React native"],
     srcURL: "",
   },
+  // {
+  //   title: "Project Title",
+  //   subTitle:
+  //     "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima nobis quia et.",
+  //   thumbnail: "../img/projects-default.jpg",
+  //   description:
+  //     "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi placeat magnam eveniet accusamus tenetur maxime aspernatur deleniti rerum praesentium ducimus minima facilis consectetur expedita, mollitia molestias qui dolorem quam laudantium. Repellendus sunt harum fugiat natus. dolor sit amet consectetur, adipisicing elit. Minima nobis quia et.",
+  //   techStack: ["Stack"],
+  //   srcURL: "",
+  // },
   // Add more objects for additional projectData here
 ];
 
@@ -994,7 +991,7 @@ function loadContent(projectData) {
        <div class='content'>{description}</div>
        <div class='technologies'>Tech Stack: ${techStackData}</div>
        <div class="card-buttons">
-          <a href="https://github.com/LakshanRukantha" target="_blank" class="card-btn" style="float: right; height: 2.35rem; color: #fff; background-color: #68d372; padding: .375rem .75rem; border-radius: .25rem;"><i class="fa-regular fa-file-code"></i>Source Code</a>
+          <a href="https://github.com/stonefadel02" target="_blank" class="card-btn" style="float: right; height: 2.35rem; color: #fff; background-color: #68d372; padding: .375rem .75rem; border-radius: .25rem;"><i class="fa-regular fa-file-code"></i>Source Code</a>
           <a href="{srcURL}" target="_blank" class="card-btn" style="float: right; color: #fff; background-color: #68d372; padding: .375rem .75rem; border-radius: .25rem;"><i class="fa-solid fa-display"></i>View Demo</a>
       </div>`,
       data
